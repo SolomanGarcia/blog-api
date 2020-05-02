ce76d01a7020ab92f85c345aa41c9ff0831e7fe3
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-02 17:55:07 -0600

    setup GET request

--- api/models/posts.js
+++ api/models/posts.js
@@ -1,9 +1,10 @@
-
+const PATH = './data.json';
+const fs = require('fs');
 
 class Post {
 
   get() {
-
+    return this.readData();
   }
 
   getIndividualBlog() {
@@ -11,7 +12,13 @@ class Post {
   }
 
   add() {
-    
+
+  }
+
+  readData() {
+    let rawData = fs.readFileSync(PATH);
+    let posts = JSON.parse(rawData);
+    return posts;
   }
 
 }
