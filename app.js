15cbcaae5c5a5dfcf910d824960e8b97aba664b9
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-03 07:40:28 -0600

    create POST route

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const Post = require('./api/models/posts');
 const postsData = new Post();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.use('./uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 app.get('/api/posts', (req, res) => {
   res.status(200).send(postsData.get());
@@ -22,6 +24,18 @@ app.get('/api/posts/:post_id', (req, res) => {
   } else {
     res.status(404).send('Not Found');
   }
+});
+
+app.post('/api/posts', (req, res) => {
+  const newPost = {
+    'id': `${Date.now()}`,
+    'title': req.body.title,
+    'content': req.body.content,
+    'post_image': req.body['post-image'],
+    'added_date': `${Date.now()}`
+  }
+  postsData.add(newPost);
+  res.status(201).send(newPost);;
 });
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
