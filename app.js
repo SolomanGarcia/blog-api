af310075a21ddd0df2362cdaa4390ba9b56b386d
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-02 18:08:06 -0600

    add get request for id

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/api/posts', (req, res) => {
   res.status(200).send(postsData.get());
 });
 
+app.get('/api/posts/:post_id', (req, res) => {
+  const postId = req.params.post_id;
+});
+
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
